48ecff875bb86ae104e9f21033691e487148cf56
Bruno Almeida <bruno.almeida.silveira@gmail.com>
2016-04-11 15:28:15 -0300

    1# - Button which controls objects' hidden

---
+++ 1#/README.md
@@ -0,0 +1,3 @@
+-First App-
+
+A button in the center of screen, when you hited, the button dissapear and appear in screen some cool images
---
+++ 1#/SuperCool/SuperCool/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Bruno Silveira on 4/11/16.
+//  Copyright © 2016 Bruno Silveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolBG: UIImageView!
+    @IBOutlet weak var uncoolButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func makeMeNotSoUncool(sender: AnyObject) {
+        coolLogo.hidden = false
+        coolBG.hidden = false
+        uncoolButton.hidden = true
+    }
+
+}
+
