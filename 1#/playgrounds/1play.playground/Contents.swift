2e15f9f15eafe6f646ebe73bdf156c4301f1bedd
Bruno Almeida <bruno.almeida.silveira@gmail.com>
2016-04-11 17:59:27 -0300

    1 play - var, let, put a type in variable

---
+++ 1#/playgrounds/1play.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var number = 20
+
+//var eh uma variable
+var decimal = 50.5
+
+decimal = 20.23
+
+//let eh uma constant
+let newDecimal = 40.5
+
+var account1 = 0.0, account2 = 1.1, account3 = 3.6
+
+var newMessage: String
+newMessage = "hi, this is a new message"
+
+var red, green, blue: Double
+
+red = 50
+green = 50.05
