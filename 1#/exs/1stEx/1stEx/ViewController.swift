30b208a87e75a1a318375a2f8489bf5c546a2036
Bruno Almeida <bruno.almeida.silveira@gmail.com>
2016-04-15 19:58:53 -0300

    first exercise not finished

---
+++ 1#/exs/1stEx/1stEx/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  1stEx
+//
+//  Created by Bruno Silveira on 4/15/16.
+//  Copyright © 2016 Bruno Silveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var blueImage: UIImageView!
+    @IBOutlet weak var redImage: UIImageView!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func hideBlueImage(sender: UIButton) {
+        blueImage.hidden = !blueImage.hidden
+        //blueButton.setTitle("unhide Blue", forState: .Normal)
+    }
+
+}
+
