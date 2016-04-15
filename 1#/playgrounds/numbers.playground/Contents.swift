0c5cef68456e66bb05873ab184180e58c45f0740
Bruno Almeida <bruno.almeida.silveira@gmail.com>
2016-04-15 19:16:46 -0300

    numbers and type Safety - int, double, int32, ...

---
+++ 1#/playgrounds/numbers.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Integer
+
+var myBankAccount: Int = -500
+
+var myAge: UInt = 22
+
+var bigNumber: Int64 = 2147482477
+
+var aNum = 6
+
+var myFloat: Float = 50.123456789
+var myDouble: Double = 50.123456789
+
+//for default this will be a double
+var mult = myFloat * Float(myDouble)
+
+var bVal: Float = 5.7
+mult = bVal
+
+
+
+
