39235202a7560ff9056357fb2d060480b79d721b
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2016-04-16 00:13:59 -0300

    include a playground with some tests with variables

---
+++ 1#/exs/2ndExs.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var firstVar = 15
+var secondVar = 5
+var sum = firstVar + secondVar
+
+var resultOfEquation = ((sum * firstVar) / 5) - firstVar
+
+var fifthVar = firstVar - secondVar
+var sixthVar = firstVar / secondVar
+
+var a = "abv", b = 3, c = 4.5
+let x = 3.3, y = "abbc", z = 99
+
+var firstDoubleVar : Double
+var firstIntVar : Int = 423
+var firstStringVar : String = "Hello"
+var secondStringVar : String = "World"
+
+var helloWorld = firstStringVar + " " + secondStringVar
+var helloWorld2 = "\(firstStringVar) \(secondStringVar)"
+
+
