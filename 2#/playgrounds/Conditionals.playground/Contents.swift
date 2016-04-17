9a930d594b05f9cdac723547d1dd02df12ac7b8e
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2016-04-17 00:03:19 -0300

    Playground with conditionals and booleans

---
+++ 2#/playgrounds/Conditionals.playground/Contents.swift
@@ -0,0 +1,51 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var isMyHouseOnFire: Bool = false
+var anotherBool = true
+
+if isMyHouseOnFire {
+    print("pls someone get me some water!")
+} else {
+    print("someone get some fire for my house!")
+}
+
+var result = true == true
+result = true == false
+result = false == false
+
+var accountTotal = 300.33
+var newGame = 59.99
+
+if accountTotal >= newGame {
+    print("I just purchased the game!")
+} else {
+    print("I dont have enough money")
+}
+
+var name = "Bruno"
+
+if name == "Bruno" {
+    print("your name is Bruno")
+} else {
+    print("your name is not Bruno")
+}
+
+var numberA = 25
+var numberB = 40
+var numberC = 45
+var numberD = 50
+var finalNumber = 100
+
+if numberA == finalNumber {
+    print(numberA)
+} else if numberB == finalNumber {
+    print(numberB)
+} else if numberC == finalNumber {
+    print(numberC)
+} else {
+    print("None of the values were equal to finalNumber")
+}
+s
+
