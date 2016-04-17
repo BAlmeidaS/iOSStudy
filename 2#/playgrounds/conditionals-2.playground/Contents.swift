e2e0ef228369d46aa9866bd77fb616aa06a1edea
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2016-04-17 00:36:51 -0300

    playground with more about conditionals, AND and OR

---
+++ 2#/playgrounds/conditionals-2.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAccount = 1000.0
+var myFriendsAccount = 2000.00
+var myFriendsFriendsAccount = 3000.00
+
+if myAccount > 900 && myFriendsAccount > 1500 && myFriendsFriendsAccount > 2000 {
+    print("We have got moocha moola!")
+}
+
+var playerAAlive = true
+var playerBAlive = true
+var playerCAlive = true
+
+if playerAAlive == false || !playerBAlive || playerCAlive == false {
+    print("One of your team member is down!")
+}
+
+//Can i retire??
+var age = 62
+var account = 100000
+var richUncleThatDiedWhoLeftMeAnInheritance = true
+
+if account >= 70000 && age >= 60 || richUncleThatDiedWhoLeftMeAnInheritance == true {
+   print("You can retire")
+}
+
